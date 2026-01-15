6e83d11e5b5cc0c25ebcc501dc91bf0025303bec
ari <61393419+eftpmc@users.noreply.github.com>
2026-01-14 19:57:12 -0500

    test

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const {
-  wrapWithAudioAPIMetroConfig,
-} = require('react-native-audio-api/metro-config');
-
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = wrapWithAudioAPIMetroConfig(config);
